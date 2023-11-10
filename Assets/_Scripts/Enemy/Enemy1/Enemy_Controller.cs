45191d4c1733bf75b9a9d6f1540e5b2799fe58d7
Kenz <pixelmantm@gmail.com>
2023-11-11 00:00:56 +0700

    Rework Enemy 1
    Testmap as reference

--- Assets/_Scripts/Enemy/Enemy1/Enemy_Controller.cs
+++ Assets/_Scripts/Enemy/Enemy1/Enemy_Controller.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_Controller : MonoBehaviour
 {
@@ -11,126 +11,20 @@ public class Enemy_Controller : MonoBehaviour
     public Enemy_Animation_Controller _Animation_Controller;
 
     [Header("Attack")]
-    [SerializeField]
-    public float _attackRange = 5;
-    [SerializeField]
-    float _WaitTime = 0.1f;
     [SerializeField] GameObject _hitParticel;
 
     [Header("Movement")]
     [SerializeField] float raycastDistance = 2.0f;
-    [SerializeField] float _rotationSpeed = 8.0f;
-    [SerializeField] float _MovementSpeed = 4f;
     [SerializeField] Transform _PatrolPaths;
+    [SerializeField] NavMeshAgent _Agent;
 
     [Header("Score")]
     [SerializeField] int _Score = 30;
 
     [HideInInspector] public Transform _Target;
     public Transform _transform;
-    [HideInInspector] public Coroutine UnTarget_Coroutine;
-    [HideInInspector] public Coroutine _chasing, _patrol;
     [HideInInspector] public bool died = false;
 
-    void Start()
-    {
-        _chasing = StartCoroutine(Chasing());
-        _patrol = StartCoroutine(Patrol());
-    }
-
-    IEnumerator Patrol()
-    {
-        var waitFrame = new WaitForEndOfFrame();
-        yield return new WaitForSeconds(0);
-        if (_PatrolPaths != null && _PatrolPaths.childCount > 0)
-        {
-            int _currentPatrolPosition = 0, _PatrolDirection = 1;
-            while (!died)
-            {
-                yield return new WaitWhile(() => _Target != null);
-
-                if (_currentPatrolPosition < 0)
-                {
-                    _currentPatrolPosition = 0;
-                    _PatrolDirection = 1;
-                }
-                else if (_currentPatrolPosition > _PatrolPaths.childCount - 1)
-                {
-                    _currentPatrolPosition = _PatrolPaths.childCount - 1;
-                    _PatrolDirection = -1;
-                }
-                Transform Pos = _PatrolPaths.GetChild(_currentPatrolPosition);
-                Vector3 Location = Pos.position;
-                while (Vector3.Distance(_transform.position, Location) > 1 && _Target == null && !died)
-                {
-                    Location.y = _transform.position.y;
-                    Vector3 LookDirection = Location - _transform.position;
-                    Quaternion lookAt = Quaternion.LookRotation(LookDirection);
-                    Quaternion rot = Quaternion.Lerp(_transform.rotation, lookAt, Time.deltaTime * _rotationSpeed);
-                    _transform.rotation = rot;
-                    _transform.Translate(Time.deltaTime * _MovementSpeed * Vector3.forward);
-                    _animator.SetBool(Enemy_Animation.Walk, true);
-
-                    yield return waitFrame;
-                }
-                _currentPatrolPosition += 1 * _PatrolDirection;
-
-            }
-        }
-    }
-    public IEnumerator Chasing()
-    {
-        var waitFrame = new WaitForEndOfFrame();
-        var waitforSec = new WaitForSeconds(_WaitTime);
-        yield return new WaitForSeconds(0);
-        while (!died)
-        {
-            yield return new WaitUntil(() => _Target != null);
-            _animator.SetTrigger(Enemy_Animation.Taunt);
-            yield return new WaitWhile(() => _Animation_Controller.isBusy);
-            while (_Target != null && !died)
-            {
-                //animation
-                if (_Animation_Controller.isBusy)
-                {
-                    yield return new WaitWhile(() => _Animation_Controller.isBusy);
-                    _animator.SetBool(Enemy_Animation.Attack, false);
-                    float speed = _animator.speed;
-                    _animator.speed = 0;
-                    yield return waitforSec;
-                    _animator.speed = speed;
-                }
-                _animator.SetBool(Enemy_Animation.Walk, true);
-
-
-                bool isPathAvailable = isBlockInfrontExist();
-
-                if (isPathAvailable && _Target != null)
-                {
-                    Vector3 Location = _Target.position;
-                    Location.y = _transform.position.y;
-                    Vector3 LookDirection = Location - _transform.position;
-                    Quaternion lookAt = Quaternion.LookRotation(LookDirection);
-                    Quaternion rot = Quaternion.Lerp(_transform.rotation, lookAt, Time.deltaTime * _rotationSpeed);
-                    _transform.rotation = rot;
-                    _transform.Translate(Time.deltaTime * _MovementSpeed * Vector3.forward);
-                    _animator.SetBool(Enemy_Animation.Walk, true);
-                }
-                else
-                {
-                    _Target = null;
-                }
-
-                yield return waitFrame;
-            }
-            _animator.SetBool(Enemy_Animation.Walk, false);
-        }
-    }
-    public IEnumerator UnChasing()
-    {
-        yield return new WaitForSeconds(10f);
-        _Target = null;
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (died) return;
@@ -144,9 +38,8 @@ public class Enemy_Controller : MonoBehaviour
     public void GoDie()
     {
         FindFirstObjectByType<Player_Control>()._score += _Score;
+        _Agent.isStopped = true;
         Instantiate(_hitParticel, transform.position, Quaternion.identity, transform);
-        if (_patrol != null) StopCoroutine(_patrol);
-        if (_chasing != null) StopCoroutine(_chasing);
         Destroy(gameObject, 2f);
         _animator.SetBool(Enemy_Animation.Die, true);
         // disable collider
@@ -164,27 +57,6 @@ public class Enemy_Controller : MonoBehaviour
         }
     }
 
-
-    private bool isBlockInfrontExist()
-    {
-        Vector3 raycastDirection = Vector3.down;
-        Vector3 offset = new Vector3(0, 0, 2.2f);
-        Vector3 raycastOrigin = _transform.TransformPoint(offset);
-        RaycastHit hit;
-
-        Debug.DrawLine(transform.position, raycastDirection);
-
-        if (Physics.Raycast(raycastOrigin, raycastDirection, out hit, raycastDistance))
-        {
-            if (hit.collider.CompareTag("Ground"))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public void setPatrolPath(Transform patrolPath)
     {
         _PatrolPaths = patrolPath;
@@ -192,14 +64,76 @@ public class Enemy_Controller : MonoBehaviour
 
     private void Update()
     {
-        Vector3 raycastDirection = Vector3.down;
-        Vector3 offset = new Vector3(0, 0, 2.2f); // Adjust the values as needed to position it behind the character.
-        Vector3 raycastOrigin = _transform.TransformPoint(offset);
-        RaycastHit hit;
-
-        Debug.DrawLine(raycastOrigin, raycastOrigin + raycastDirection * raycastDistance, Color.red);
+        if(died) return;
+        Move();
     }
 
+    public void Move()
+    {
+        if (_Animation_Controller.isBusy)
+        {
+            _Agent.isStopped = true;
+            return;
+        }
+
+        if (_Target != null && IsDestinationReachable(_Target.position))
+            onChase();
+        else
+            onPatrol();
+    }
+
+    Transform PosPatrol;
+    bool onChasing = false;
+    public void onPatrol()
+    {
+        onChasing = false;
+        if(_Agent.remainingDistance < .5f)
+        {
+            PosPatrol = _PatrolPaths.GetChild(Random.Range(0, _PatrolPaths.childCount));
+        }
+        Vector3 Location = PosPatrol.position;
+        _Agent.SetDestination(Location);
+        if (IsDestinationReachable(Location))
+        {
+            _Agent.isStopped = false;
+            _animator.SetBool(Enemy_Animation.Walk, true);
+        }
+        else
+        {
+            _Agent.isStopped = true;
+            _animator.SetBool(Enemy_Animation.Walk, false);
+        }
+    }
+    public void onChase()
+    {
+        if (!onChasing)
+        {
+            _animator.SetTrigger("Taunt");
+            onChasing = true;
+            return;
+        }
+        
+        _Agent.SetDestination(_Target.position);
+        if (IsDestinationReachable(_Target.position))
+        {
+            _Agent.isStopped = false;
+            _animator.SetBool(Enemy_Animation.Walk, true);
+        }
+        else
+        {
+            _Agent.isStopped = true;
+            _animator.SetBool(Enemy_Animation.Walk, false);
+        }
+    }
+    bool IsDestinationReachable(Vector3 destination)
+    {
+        // Perform a NavMeshPath calculation to check if the destination is reachable
+        NavMeshPath path = new NavMeshPath();
+        _Agent.CalculatePath(destination, path);
+
+        // Check if the path status is not PathPartial (i.e., the destination is reachable)
+        return path.status != NavMeshPathStatus.PathPartial;
+    }
 }
 public class Enemy_Animation
 {
--- Assets/_Scripts/Enemy/Enemy1/Enemy_Vision_Control.cs
+++ Assets/_Scripts/Enemy/Enemy1/Enemy_Vision_Control.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Enemy_Vision_Control : MonoBehaviour
 {
     [SerializeField] Enemy_Controller controller;
+    [SerializeField] Transform enemy;
+
+    Transform _transform;
+    private void Awake()
+    {
+        _transform = transform;
+    }
+    private void LateUpdate()
+    {
+        if (controller.died)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _transform.position = enemy.position;
+        _transform.rotation = enemy.rotation;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (controller.died) return;
         if (other.tag == "Player")
         {
-            if (controller.UnTarget_Coroutine != null) StopCoroutine(controller.UnTarget_Coroutine);
             controller._Target = other.transform;
         }
     }
+
     private void OnTriggerStay(Collider other)
     {
         if (controller.died) return;
@@ -25,20 +42,12 @@ public class Enemy_Vision_Control : MonoBehaviour
             Vector3 e_dist = transform.position;
             e_dist.y = 0;
 
-            if (Vector3.Distance(e_dist, p_dist) < controller._attackRange)
-            {
-                controller._animator.SetBool(Enemy_Animation.Walk, false);
-                controller._animator.SetBool(Enemy_Animation.Attack, true);
-            }
             controller._Target = other.transform;
         }
 
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            controller.UnTarget_Coroutine = StartCoroutine(controller.UnChasing());
-        }
+
     }
 }
